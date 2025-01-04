3d6057317ffe24f35011a5f2941cc882803948c4
Sarthak <saarsaach30@gmail.com>
2025-01-04 10:55:08 +0530

    Create _document.tsx

---
+++ nextJS-code/src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Social links profile</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
